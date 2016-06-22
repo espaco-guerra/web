087b2dfcb46e0d84b4d2dfb22128ea07177c957d
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-06-22 11:56:36 -0700

    Fixing konacha config to use Chrome
    Due to a problem with Firefox 47, Konacha tests don't report correctly
    to the command line. Issue is
    https://github.com/jfirebaugh/konacha/issues/228.
    While it is not fixed, using chromedriver. To use firefox 47, simply use
    the selenium_marionette driver for Konacha in the initializer.

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,4 +1,10 @@
 # encoding: UTF-8
 Konacha.configure do |config|
-  config.driver = :selenium
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
+  Capybara.register_driver :selenium_marionette do |app|
+    Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true)
+  end
+  config.driver = :selenium_chrome
 end if defined?(Konacha)
--- dev.sh
+++ dev.sh
@@ -1,4 +1,31 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set e
+
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${MY_DIR}
+
+if [[ -z `which ruby` ]]; then
+  echo "Missing ruby in your path. Please install the correct version and try again" && exit 1
+fi
+
+if [[ -z `which gem` ]]; then
+  echo "Missing rubygems in your path. Please install the correct version and try again" && exit 1
+fi
+
+if [[ -z `which npm` ]]; then
+  echo "Installing npm..."
+  ((brew --version > /dev/null) && (brew install npm > /dev/null))
+  ((apt-get --version > /dev/null) && (apt-get install -y nodejs npm))
+fi
+
 npm install
+
+if [[ -z `which bundle` ]]; then
+  echo "Installing bundler..."
+  gem --version &> /dev/null && gem install bundler &> /dev/null
+fi
 bundle
+
+export PATH=${MY_DIR}/bin:${PATH}
 bundle exec foreman start -f Procfile.dev
