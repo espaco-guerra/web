4c3898d10083df948ba03fe3e0a6bc625e04c9b7
Hugo Corbucci <hugo.corbucci@gmail.com>
2016-10-31 17:26:41 -0700

    Updating gems
    Had to update the driver's executables too due to chrome and firefox
    new versions.
    Also fixing a new rubocop rule.

--- config/initializers/konacha.rb
+++ config/initializers/konacha.rb
@@ -1,10 +1,12 @@
 # encoding: UTF-8
-Konacha.configure do |config|
-  Capybara.register_driver :selenium_chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
+if defined?(Konacha)
+  Konacha.configure do |config|
+    Capybara.register_driver :selenium_chrome do |app|
+      Capybara::Selenium::Driver.new(app, browser: :chrome)
+    end
+    Capybara.register_driver :selenium_marionette do |app|
+      Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true)
+    end
+    config.driver = :selenium_chrome
   end
-  Capybara.register_driver :selenium_marionette do |app|
-    Capybara::Selenium::Driver.new(app, browser: :firefox, marionette: true)
-  end
-  config.driver = :selenium_chrome
-end if defined?(Konacha)
+end
